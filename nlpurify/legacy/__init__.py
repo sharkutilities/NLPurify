2418e0a209f497f691f464d06856fcd0c0f64641
ZenithClown <debmalyapramanik.005@gmail.com>
2024-08-18 21:47:49 +0530

    ♻️ add cautionary block when browsing legacy submodule docs

--- nlpurify/legacy/__init__.py
+++ nlpurify/legacy/__init__.py
@@ -9,6 +9,11 @@ advanced collection of features. However the existing codes from the
 legacy submodule unless dependent codes are gradually migrated.
 
 More Information: `Issue #5 <https://github.com/sharkutilities/NLPurify/issues/5>`_
+on code migrations and submodule details.
+
+.. caution::
+    The documentation does not follow PEP-8 convention, and is not maintained
+    properly. This submodule is kept only as a precautionary submodule.
 """
 
 from nlpurify.legacy.nlp_utils import * # noqa: F401, F403 # pyright: ignore[reportMissingImports]
