3228406087bdff7e15d05bc5cd2079a97125842f
ZenithClown <debmalyapramanik.005@gmail.com>
2024-08-18 16:57:41 +0530

    🎉 bump code to 0.0.1.dev1

--- nlpurify/__init__.py
+++ nlpurify/__init__.py
@@ -10,7 +10,7 @@ one-stop solution library oriented towards text cleaning.
 
 # ? package follows https://peps.python.org/pep-0440/
 # ? https://python-semver.readthedocs.io/en/latest/advanced/convert-pypi-to-semver.html
-__version__ = "0.0.1.dev0"
+__version__ = "0.0.1.dev1"
 
 # init-time options registrations
 from nlpurify.normalize import normalizeText
