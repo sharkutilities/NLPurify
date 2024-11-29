f6d83d35a70c5e3f8a6aad7d497cc3051aaa34df
E33605 <debmalyapramanik.005@gmail.com>
2024-11-29 17:41:35 +0530

    📝 update evaluate documentation

--- nlpurify/scoring/baseclass.py
+++ nlpurify/scoring/baseclass.py
@@ -25,7 +25,7 @@ class BaseLogicalOperator(ABC):
 
     def evaluate(self, thresh : int, logic : str, operator : str = ">="):
         """
-        Evaluate the Final Score using Operators
+        Evaluate the Final Score using Logical Operators
 
         The operator like :attr:`>=`, :attr:`<=` is dynamic and thus
         provides additional controls to the logical operations as it
@@ -33,6 +33,13 @@ class BaseLogicalOperator(ABC):
         curve for sequence matching. The operator is used internally
         and is evaluated using the :func:`eval()` to determine the
         final result and provide the score.
+
+        ..versionchanged:: v2.0.0 gh#19 gh#18 gh#20
+        The function ``scores()`` is now used to calculate the
+        individual scores and the function ``evaluate()`` is now used
+        to evaluate the final score using logical operators. The
+        method is now available as a abstract method in the baseclass
+        and returns an iterable values.
 
         :type  logic: str
         :param logic: The logical operator which is either :attr:`all`
